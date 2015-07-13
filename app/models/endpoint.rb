4ef205db4dd86788178731c2d6f21435beed63d1
Justin Baumgartner <justbaum30@gmail.com>
2015-07-12 20:39:44 -0500

    Move status change notification logic to Endpoint instead of Request.

--- app/models/endpoint.rb
+++ app/models/endpoint.rb
@@ -6,11 +6,67 @@ class Endpoint < ActiveRecord::Base
     self.requests.where(:status => true).size
   end
 
-  def status
-    self.number_successful_requests > 0
-  end
-
   def status_css_class
     self.status ? 'good' : 'bad'
+  end
+
+  def notify_status_changed
+    old_status = self.status
+    new_status = self.number_successful_requests > 0
+    self.update_columns(status: new_status)
+
+    if old_status != new_status
+      logger.debug 'The status of the endpoint has changed'
+
+      #make_parse_api_request
+      if self.id == 2235 # TODO: Don't hardcode endpoint id for flag
+        if new_status
+          lower_flag
+        else
+          hoist_flag
+        end
+      end
+    end
+  end
+
+  ### Network requests ###
+
+  private def make_parse_api_request
+    header = {
+        'Content-Type' => 'application/json',
+        'X-Parse-Application-Id' => 'rDo0wJWA9ZOvf3oVhx6UW2b5YnVHsxo2ToicQ1GE',
+        'X-Parse-REST-API-Key' => 'UR4t91a2yutiqjWp7lyYMNf0DYqUadMen0fVKZiK'
+    }
+    payload = {
+        'channels' => ['crows-nest'],
+        'data' => {
+            'alert' => ''
+        }
+    }
+
+    https = Net::HTTP.new('api.parse.com', 443)
+    https.use_ssl = true
+    req = Net::HTTP::Post.new('/1/push', header)
+    req.body = payload.to_json
+    res = https.request(req)
+    puts "Response #{res.code} #{res.message}: #{res.body}"
+  end
+
+  private def hoist_flag
+    RubySpark.configuration do |config|
+      config.access_token = 'd96b9f0c4d42cbfeab281f55abf86de26669e257'
+    end
+
+    core = RubySpark::Core.new('55ff6e065075555352311787')
+    core.function('hoist', 'hoistDiscover')
+  end
+
+  private def lower_flag
+    RubySpark.configuration do |config|
+      config.access_token = 'd96b9f0c4d42cbfeab281f55abf86de26669e257'
+    end
+
+    core = RubySpark::Core.new('55ff6e065075555352311787')
+    core.function('hoist', 'lowerDiscover')
   end
 end
--- app/models/request.rb
+++ app/models/request.rb
@@ -1,59 +1,13 @@
 class Request < ActiveRecord::Base
   belongs_to :endpoint
 
-  before_update :status_has_changed, :if => :status_changed?
+  after_update :after_changed_test, :if => :status_changed?
 
-  def status_has_changed
-    make_parse_api_request
-    if self.id == 5
-      if self.status
-        lower_flag
-      else
-        hoist_flag
-      end
-    end
+  def after_changed_test
+    self.endpoint.notify_status_changed
   end
 
   def status_css_class
     self.status ? 'good' : 'bad'
-  end
-
-  def make_parse_api_request
-    header = {
-        'Content-Type' => 'application/json',
-        'X-Parse-Application-Id' => 'rDo0wJWA9ZOvf3oVhx6UW2b5YnVHsxo2ToicQ1GE',
-        'X-Parse-REST-API-Key' => 'UR4t91a2yutiqjWp7lyYMNf0DYqUadMen0fVKZiK'
-    }
-    payload = {
-        'channels' => ['crows-nest'],
-        'data' => {
-            'alert' => ''
-        }
-    }
-
-    https = Net::HTTP.new('api.parse.com', 443)
-    https.use_ssl = true
-    req = Net::HTTP::Post.new('/1/push', header)
-    req.body = payload.to_json
-    res = https.request(req)
-    puts "Response #{res.code} #{res.message}: #{res.body}"
-  end
-
-  def hoist_flag
-    RubySpark.configuration do |config|
-      config.access_token = 'd96b9f0c4d42cbfeab281f55abf86de26669e257'
-    end
-
-    core = RubySpark::Core.new('55ff6e065075555352311787')
-    core.function('hoist', 'hoistDiscover')
-  end
-
-  def lower_flag
-    RubySpark.configuration do |config|
-      config.access_token = 'd96b9f0c4d42cbfeab281f55abf86de26669e257'
-    end
-
-    core = RubySpark::Core.new('55ff6e065075555352311787')
-    core.function('hoist', 'lowerDiscover')
   end
 end
